45b48a1d5d0d68290465328c8fe170be00c1efa2
Gurpreet Singh <100257273+Gurpreet-s0@users.noreply.github.com>
2026-07-11 00:52:31 +0530

    server deployement change

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,6 +2,8 @@ const app = require("./src/app")
 const connectToDb = require("./src/Database/database")
 connectToDb()
 
-app.listen("3000",()=>{
-    console.log("server is running on 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
